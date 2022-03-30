114c95368645fb8068c39464dd920ea877d45c25
Mikedeejay2 <mikedeejay2@gmail.com>
2022-03-30 13:57:35 -0400

    Begin code

--- src/main/java/com/mikedeejay2/jsee/JSEE.java
+++ src/main/java/com/mikedeejay2/jsee/JSEE.java
@@ -1,15 +1,32 @@
 package com.mikedeejay2.jsee;
 
+import java.lang.invoke.MethodHandles;
+import java.lang.reflect.Field;
+
 public class JSEE {
+    static {
+//        MethodHandles.privateLookupIn()
+    }
+
+    private static boolean enabled;
+
     public static boolean setEnabled(boolean value) {
-        return value ? enable() : disable();
-    }
-
-    private static boolean enable() {
+        if(value == enabled) return false;
+        try {
+            if(value) enable();
+            else disable();
+        } catch(Exception e) {
+            e.printStackTrace();
+            return false;
+        } finally {
+            enabled = true;
+        }
         return true;
     }
 
-    private static boolean disable() {
-        return true;
+    private static void enable() throws ReflectiveOperationException {
+    }
+
+    private static void disable() throws ReflectiveOperationException {
     }
 }
--- src/test/java/com/mikedeejay2/jsee/JSEETester.java
+++ src/test/java/com/mikedeejay2/jsee/JSEETester.java
@@ -1,8 +1,20 @@
 package com.mikedeejay2.jsee;
 
+import java.lang.reflect.Field;
+
 public class JSEETester {
     public static void main(String[] args) {
         JSEE.setEnabled(true);
-        System.out.println("Success");
+        System.out.println("Enabled JSEE");
+
+        try {
+            Class<?> testClass = Class.class;
+            Field field = testClass.getDeclaredField("module");
+            field.setAccessible(true);
+        } catch (Exception e) {
+            e.printStackTrace();
+        } finally {
+            System.out.println("Success");
+        }
     }
 }
